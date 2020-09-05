a93c99530817043344545a78557576d2d77184e0
ali-h <iamahaalihassanarain@gmail.com>
2020-09-05 22:22:32 +0500

    editor placehold

--- JS/main.js
+++ JS/main.js
@@ -2,5 +2,28 @@
 $(() => {
   // json editor config
   const editor = document.querySelector('#editor');
-  const jar = CodeJar(editor, Prism.highlightElement, {tab: ' '.repeat(2)});
+  const highlight = editor => {
+    // highlight.js does not trims old tags,
+    // let's do it by this hack.
+    editor.textContent = editor.textContent
+    hljs.highlightBlock(editor)
+  }
+  const jar = CodeJar(editor, highlight, {tab: ' '.repeat(2)});
+  
+  // create editor placeholder
+  var isEdited = false
+  $("#editor").focus(function() {
+    if (!isEdited == true) {
+      $("#editor").html("")
+      $("#editor").removeClass("editor_empty")
+      isEdited = true
+    }
+  })
+  $("#editor").focusout(function() {
+    if($("#editor").html() == "") {
+      $("#editor").html("JSON")
+      $("#editor").addClass("editor_empty")
+      isEdited = false
+    }
+  })
 });
